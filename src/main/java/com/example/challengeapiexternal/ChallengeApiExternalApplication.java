04ae32ecf0da90e267ac01ee861e5c156556098f
Gabriel Rodrigues <gabrielrodrigues1927@gmail.com>
2023-09-02 22:52:24 -0300

    remove mapper

--- src/main/java/com/example/challengeapiexternal/ChallengeApiExternalApplication.java
+++ src/main/java/com/example/challengeapiexternal/ChallengeApiExternalApplication.java
@@ -1,16 +1,10 @@
 package com.example.challengeapiexternal;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ChallengeApiExternalApplication {
-	@Bean
-	public ModelMapper modelMapper (){
-		return new ModelMapper();
-	}
 	public static void main(String[] args) {
 		SpringApplication.run(ChallengeApiExternalApplication.class, args);
 	}
